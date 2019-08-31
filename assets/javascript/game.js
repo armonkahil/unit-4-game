550edfe18a549d1e6f0517a2f360b21b7676238f
South Matic <Southmatic729@gmail.com>
2019-08-30 20:51:47 -0400

    got the player cards to display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,146 @@
-//instructions
-//the goal of the game is to kill all the other characters by fighting them
-//pick a character to fight with
-//the other 3 characters become enenmies
-//your character w
-//obi wan hits with 8 damage, with every attack increases by 1.
-//darth Maul attack power is 25 damage
-//add a restart button
 
-//120 100 150 180
-
-
-$(document).ready(function(){
+$(document).ready(function() {
     // $("*").hide();
-    // jQuery methods go here...
-    // $('.jquery-background-video').bgVideo({fadeIn: 2000});
+    
 
     var obi = {
       name: "Obi-Wan",
+      nickname: "Obi",
       health: 120,
       attackPW: 8,
       counterPW: 8,
-      cardImg: "src = ../assets/images/obiWan.jpeg"
+      cardImg: "../assets/images/Obi1.jpg"
     };
 
     var luke = {
       name: "Luke Skywalker",
+      nickname : "Luke",
       health: 100,
       attackPW: 5,
       counterPW: 5,
-      cardImg: "../assets/images/Luke_skywalker.jpg"
+      cardImg: "../assets/images/Luke1.jpg"
     };
 
     var vader = {
       name: "Darth Vader",
+      nickname: "Vader",
       health: 150,
       attackPW: 20,
       counterPW: 20,
-      cardImg: "../assets/images/vader.jpeg"
+      cardImg: "../assets/images/Vader1.jpg"
     };
 
     var sidious = {
       name: "Darth Sidious",
+      nickname: "Sidious",
       health: 180,
       attackPW: 25,
       counterPW: 25,
-      cardImg: "../assets/images/Emperor_RotJ.png"
+      cardImg: "../assets/images/Sith-Primer-Sidious.jpg"
     };
 
-function SetGame (player) {
-  var newDiv = $("<div>");
-  var newIMG = $("<img>");
-  var newPname = $("<p>");
-  var newPhealth = $("<p>");
-  var pl = (''"#" + player.name "');
-  newPname = player.name;
-  newPhealth = player.health;
-  newDiv.attr("id", player.name);
-  newIMG.attr("src", player.cardImg);
-  $("#playerBoard").append(newDiv);
-  $("#playerBoard").append(newPname);
+
+var players = [obi, luke, vader, sidious];
+playersLeft = [];
+// console.log(playersLeft);
+// var dummyOBJ = {
+//   name: "Savage Oppress",
+//   nickname: "Savage",
+//   health: 300,
+//   attackPW: 50,
+//   counterPW: 200,
+//   cardImg: "../assets/images/savage.jpg"
+// };
+
+var dummyOBJ
+
+
+function buildChar (charID){
+ 
+  console.log("This is the value buildChar got " + charID);
+  for (i=0; i < players.length; i++){
+    if (charID === players[i].nickname) {
+      dummyOBJ = players[i];
+      
+    } else {
+
+      console.log(players[i] + " is being added to playersLeft.")
+      playersLeft.push(players[i]);
+      console.log(playersLeft);
+      
+    }
+    setting("#userAttacks", dummyOBJ);  
+    setting("#enemiesSelect", playersLeft);
+      
   
+      console.log("This is who's left" + playersLeft);
+      
+     
+  }
+}
+ //sets the board of players
+function reset(target) {
+  $(target).children().remove();
+
+   for (var i = 0; i < players.length; i++) { 
+            $(target).append("<div />");
+            $(target + " div:last-child").addClass("card text-center yourChar");
+            $(target + " div:last-child").attr("id", players[i].nickname);
+            $(target + " div:last-child").append("<p>" + players[i].name + "</p>");
+            $(target + " div:last-child").append("<img />");
+            $(target + " img:last-child").attr("class", "rounded img-fluid");
+            $(target + " img:last-child").attr("src", players[i].cardImg);
+            $(target + " img:last-child").attr("width", "160");
+            $(target + " div:last-child").append("<p>" + players[i].health + "</p>");
+            $(target + " idv:last-child").append();
+    }
+  }
 
   
-  newDiv.attr("class", "card text-center");
-  $("#playerBoard").append(newIMG);
-  $("#playerBoard").append(newPhealth);
+
+function setting (target,array1) {
+  var selection = array1;
+  $(target).children().remove();
+     console.log("this is the target div ", target);
+     console.log("this is the array received ", selection);
+     
+     for (var i=0; i < selection.length; i++) {  
+            
+          $(target).append("<div />");
+          $(target + " div:last-child").addClass("card text-center yourChar");
+          $(target + " div:last-child").attr("id", selection[i].nickname);
+          $(target + " div:last-child").append("<p>" + selection[i].name + "</p>");
+          $(target + " div:last-child").append("<img />");
+          $(target + " img:last-child").attr("class", "rounded img-fluid");
+          $(target + " img:last-child").attr("src", selection[i].cardImg);
+          $(target + " img:last-child").attr("width", "160");
+          $(target + " div:last-child").append("<p>" + selection[i].health + "</p>");
+          $(target + " idv:last-child").append();
+  }
+  }
+
+
+//TODO: I need a function that will take the id and return a variable equal to the character selected.
+
+
+
+
+
+
+
+function CharSelect () {
+  $(".yourChar").on('click', function() {
+        console.log("i clicked " + this.id);
+        
+      setting("#playerSelect",buildChar(this.id));
+  });
 }
 
-SetGame(luke);
+ reset("#playerSelect");
+ reset("#enemiesSelect");
 
+ CharSelect()
 
-
+  
 
 });
