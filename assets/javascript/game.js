b5d7029dd4b8b084bc45464ad1e4012caf9f0661
South Matic <Southmatic729@gmail.com>
2019-08-31 21:31:01 -0400

    redesigned html structure

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,18 @@
+//ensures code does not run until the page full loads
 $(document).ready(function() {
   // $("*").hide();
+  // ============================================================================
+  // Global Variables
+  // ============================================================================
+
+  // ----------------------------------------------------------------------------
+  // Character stats and attributes are stored in objects.
+  // ----------------------------------------------------------------------------
   var obi = {
     name: "Obi-Wan",
     nickname: "Obi",
     health: 120,
+    battleHealth: 0,
     attackPW: 8,
     counterPW: 8,
     cardImg: "../assets/images/Obi1.jpg",
@@ -14,6 +23,7 @@ $(document).ready(function() {
     name: "Luke Skywalker",
     nickname: "Luke",
     health: 100,
+    battleHealth: 0,
     attackPW: 5,
     counterPW: 5,
     cardImg: "../assets/images/Luke1.jpg",
@@ -24,6 +34,7 @@ $(document).ready(function() {
     name: "Darth Vader",
     nickname: "Vader",
     health: 150,
+    battleHealth: 0,
     attackPW: 20,
     counterPW: 20,
     cardImg: "../assets/images/Vader1.jpg",
@@ -34,19 +45,25 @@ $(document).ready(function() {
     name: "Darth Sidious",
     nickname: "Sidious",
     health: 180,
+    battleHealth: 0,
     attackPW: 25,
     counterPW: 25,
     cardImg: "../assets/images/Sith-Primer-Sidious.jpg",
     playAvail: true
   };
+  //array created to store character objects
+  var playersArray = [obi, luke, vader, sidious];
+  const staticArray = [obi, luke, vader, sidious];
+  //arrays for characters selected
+  var firstPlayer = [];
+  var secondPlayer = [];
+  //conditions for players selected
+  var player1Confirmed = false;
+  var player2Confirmed = false;
 
-  var players = [obi, luke, vader, sidious];
-
-  playersLeft = [];
-
-
-  var dummyOBJ = [];
-
+  // =============================================================================
+  // console reporting function
+  // =============================================================================
   function report(section) {
     var divider = "--------------------------------------------------";
     console.log(" ");
@@ -55,54 +72,10 @@ $(document).ready(function() {
     console.log(divider);
   }
 
-  function buildChar(charID) {
-    report("buildChar");
-    console.log("This is the value buildChar got " + charID);
-    for (i = 0; i < players.length; i++) {
-      if (charID === players[i].nickname && players[i].playAvail) {
-        dummyOBJ.push(players[i]);
-        players.splice(i, 1); //<-----I created a global dummy object "dummyOBJ" to r
-        // players[i].playAvail = false;
-      }
-    }
-
-    // return dummyOBJ.nickname;
-    //these functions are meant to update the board
-    //takes chosen player and sends it to be displayed in the user attack area
-    setting("#userAttacks", dummyOBJ);
-    //takes the remaining players to display in the players left section
-    resetGame("#enemiesSelect", players);
-    console.log("This is who's left" + playersLeft);
-  }
-  //sets the board of players
-  function resetGame(target) {
-    report("resetGame");
-    console.log("this is the target received", target);
-    console.log("removing children from id", target);
-    $(target)
-      .children()
-      .remove();
-    console.log("running resetGame loop for " + players.length + " times.");
-    for (var i = 0; i < players.length; i++) {
-      if (players[i].playAvail) {
-        console.log("creating div for ", target);
-        $(target).append("<div />");
-        $(target + " div:last-child").addClass("card text-center yourChar");
-        $(target + " div:last-child").attr("id", players[i].nickname);
-        $(target + " div:last-child").append("<p>" + players[i].name + "</p>");
-        $(target + " div:last-child").append("<img/>");
-        $(target + " img:last-child").attr("class", "rounded img-fluid");
-        $(target + " img:last-child").attr("src", players[i].cardImg);
-        $(target + " img:last-child").attr("width", "160");
-        $(target + " div:last-child").append(
-          "<p>" + players[i].health + "</p>"
-        );
-        $(target + " div:last-child").append();
-      }
-    }
-  }
-
-  // This is where i run into my problem....the player array of objects is now undefined.
+  // ===========================================================================
+  // display functions
+  // ===========================================================================
+  //updates display
   function setting(target, selection) {
     report("setting");
     $(target)
@@ -131,20 +104,155 @@ $(document).ready(function() {
     }
   }
 
+  //sets the board of playersArray
+  function update(target) {
+    report("resetGame");
+    //sets up game display on first run. Second run resets game
+    console.log("this is the target received", target);
+    console.log("removing children from id", target);
+    $(target)
+      .children()
+      .remove();
+    console.log(
+      "running resetGame loop for " + playersArray.length + " times."
+    );
+    for (var i = 0; i < playersArray.length; i++) {
+      if (playersArray[i].playAvail) {
+        $(target).append("<div />");
+        $(target + " div:last-child").addClass("card text-center yourChar");
+        $(target + " div:last-child").attr("id", playersArray[i].nickname);
+        $(target + " div:last-child").append(
+          "<p>" + playersArray[i].name + "</p>"
+        );
+        $(target + " div:last-child").append("<img/>");
+        $(target + " img:last-child").attr("class", "rounded img-fluid");
+        $(target + " img:last-child").attr("src", playersArray[i].cardImg);
+        $(target + " img:last-child").attr("width", "160");
+        $(target + " div:last-child").append(
+          "<p>" + playersArray[i].health + "</p>"
+        );
+        $(target + " div:last-child").append();
+      }
+    }
+  }
+
+  //sets the board of playersArray
+  function resetGame() {
+    var resetTarget = "#playerSelect";
+    $(resetTarget)
+      .children()
+      .remove();
+
+    for (var i = 0; i < playersArray.length; i++) {
+      //resets playersArray to original stats.
+      playersArray[i] = staticArray[i];
+      $(resetTarget).append("<div />");
+      $(resetTarget + " div:last-child").addClass("card text-center yourChar");
+      $(resetTarget + " div:last-child").attr("id", playersArray[i].nickname);
+      $(resetTarget + " div:last-child").append(
+        "<p>" + playersArray[i].name + "</p>"
+      );
+      $(resetTarget + " div:last-child").append("<img/>");
+      $(resetTarget + " img:last-child").attr("class", "rounded img-fluid");
+      $(resetTarget + " img:last-child").attr("src", playersArray[i].cardImg);
+      $(resetTarget + " img:last-child").attr("width", "160");
+      $(resetTarget + " div:last-child").append(
+        "<p>" + playersArray[i].health + "</p>"
+      );
+      $(resetTarget + " div:last-child").append();
+    }
+    console.log("this is the static array ", staticArray);
+    console.log("this is the players array ", playersArray);
+    player1Confirmed = false;
+    player2Confirmed = false;
+  }
+
+
+
+
+  // =============================================================================
+  // Game functionality
+  // =============================================================================
+  // =============================================================================
+  //function for pushing selected Character into dummy object array.
+  // =============================================================================
+  function buildChar(charID) {
+    report("buildChar");
+    console.log("This is the value buildChar got " + charID);
+      if (!player1Confirmed && !player2Confirmed) {
+        console.log("no character selected");
+        return
+      } else if (player1Confirmed && !player2Confirmed) {
+          //if the first player has been confirmed and the second has not
+              for (i = 0; i < playersArray.length; i++) {
+                      if (charID === playersArray[i].nickname) {
+                              playersArray[i].playAvail = false;
+                              firstPlayer.push(playersArray[i]);
+                              playersArray.splice(i, 1);
+                      }  
+
+              }
+              setting("#userAttacks",firstplayer)
+       
+      } else if (player1Confirmed && player2Confirmed) {
+        //if player 1 has been confirmed and player 2 has been confirmed
+        for (i = 0; i < playersArray.length; i++) {
+          if (charID === playersArray[i].nickname) {
+                  playersArray[i].playAvail = false;
+                  secondPlayer.push(playersArray[i]);
+                  playersArray.splice(i, 1);
+          }  
+
+        }
+        setting(secondPlayer)
+      }
+  }
+
+    // return dummyOBJ.nickname;
+    //these functions are meant to update the board
+    //takes chosen player and sends it to be displayed in the user attack area
+    setting("#userAttacks", dummyOBJ);
+    //takes the remaining playersArray to display in the playersArray left section
+    resetGame("#enemiesSelect", playersArray);
+    console.log("This is who's left" + playersArrayLeft);
+  
+
+
+
   function charSelect() {
     report("charSelect");
     $(".yourChar").on("click", function() {
-      buildChar(this.id);
-      setting("#playerSelect", dummyOBJ);
+      //if first player hasnt been selected yet...
+      if (!player1Confirmed) {
+        console.log("picked first player");
+        buildChar(this.id);
+        player1Confirmed = true;
+        // setting("#playerselect",firstPlayer);
+        //if first has been selected and second player has not been selected.
+      } else if (!player2Confirmed) {
+        console.log("picked second player");
+        player2Confirmed = true;
+        buildChar(this.id);
+      }
     });
   }
+  // =============================================================================
+  //
+  // =============================================================================
+  // =============================================================================
+  //
+  // =============================================================================
+  // =============================================================================
+  // Sets up game
+  // =============================================================================
 
-  //---------------------------------------------------------------------------------------------------------------------------------
-  //---------------------------------------------------------------------------------------------------------------------------------
-  //sets up initial divs displaying the players
-  resetGame("#playerSelect");
-  //  reset("#enemiesSelect");
+  function Game() {
+    resetGame("#playersArrayelect");
+    //  reset("#enemiesSelect");
 
-  //starts the game
-  charSelect();
+    //starts the game
+    charSelect();
+  }
+
+  Game();
 });
